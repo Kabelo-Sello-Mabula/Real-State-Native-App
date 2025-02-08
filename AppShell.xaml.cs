d3dc6e40f489da3fec97fef94153e72226d3202f
KS-IKIGAI-DEV <KS-IKIGAI-DEV@DESKTOP-90QIGEP>
2025-02-08 02:19:17 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_Real_Estate
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace MAUI_Real_Estate
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
